dc1978f6016354f9c8f50375262683e9d94c58fa
4K45H107 <fahad11.ahmed11@gmail.com>
2024-10-23 04:32:28 +0600

    Firebase setup

---
+++ src/lib/firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: "reactchat-ed5f2.firebaseapp.com",
+  projectId: "reactchat-ed5f2",
+  storageBucket: "reactchat-ed5f2.appspot.com",
+  messagingSenderId: "154253592516",
+  appId: "1:154253592516:web:27d64937a8baf2a05e0691",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const storage = getStorage(app);
